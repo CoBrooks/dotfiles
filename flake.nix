8e0e6d1d245f8a0ed644720108f66b03a73e4055
CoBrooks <cole.brooks@colebrooks.com>
2022-07-15 17:14:00 -0500

    Assorted Cleanup

--- flake.nix
+++ flake.nix
@@ -2,22 +2,15 @@
   description = "NixOS Config";
 
   inputs = {
-    # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    # Home manager
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    # Utilities for building our flake
     flake-utils.url = "github:numtide/flake-utils";
 
-    # Extra flakes for modules, packages, etc
-    hardware.url = "github:nixos/nixos-hardware"; # Convenience modules for hardware-specific quirks
-    # nur.url = "github:nix-community/NUR";              # User contributed pkgs and modules
-    # nix-colors.url = "github:misterio77/nix-colors";   # Color schemes for usage with home-manager
-    # impermanence.url = "github:riscadoa/impermanence"; # Utilities for opt-in persistance
-
+    hardware.url = "github:nixos/nixos-hardware"; 
+    
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
@@ -33,68 +26,51 @@
       system = "x86_64-linux";
 
       pkgs = import nixpkgs {
-        system = "x86_64-linux";
+        inherit system;
         overlays = [ rust-overlay.overlays.default ];
       };
 
       themes = import ./themes { inherit pkgs; };
       inherit (themes) getTheme;
       theme = getTheme { name = "gruvbox-material-dark-medium"; };
-    in
-    rec {
-      # TODO: If you want to use packages exported from other flakes, add their overlays here.
-      # They will be added to your 'pkgs'
-      overlays = {
-        default = import ./overlay; # Our own overlay
-        # nur = nur.overlay
-      };
-
-      # System configurations
-      # Accessible via 'nixos-rebuild'
+    in rec {
       nixosConfigurations = {
         cbrooks-laptop = nixosSystem {
           inherit system;
 
           modules = [
-            # >> Main NixOS configuration file <<
             ./nixos/configuration.nix
-            # Adds your custom NixOS modules
             ./modules/nixos
-            # Adds overlays
             { 
-              nixpkgs.overlays = attrValues overlays; 
-              environment.systemPackages = [ pkgs.rust-bin.stable.latest.default ];
+              environment.systemPackages = [ 
+                # Add Rust to system
+                pkgs.rust-bin.stable.latest.default 
+              ];
             }
           ];
-          # Make our inputs available to the config (for importing modules)
           specialArgs = { inherit inputs; };
         };
       };
 
-      # Home configurations
-      # Accessible via 'home-manager'
       homeConfigurations = {
         "cbrooks@cbrooks-laptop" = homeManagerConfiguration {
+          inherit pkgs;
+
           modules = [
             {
               home.username = "cbrooks";
               home.homeDirectory = "/home/cbrooks";
 
               home.stateVersion = "22.11";
+
+              nixpkgs.config.allowUnfree = true;
+
+              programs.home-manager.enable = true;
+              systemd.user.startServices = "sd-switch";
             }
-            # >> Main home-manager configuration file <<
-            ./home-manager/home.nix
-            # Adds your custom home-manager modules
             ./modules/home-manager
-            # Adds overlays
-            { nixpkgs.overlays = attrValues overlays; }
           ];
 
-          pkgs = import nixpkgs {
-            inherit system;
-          };
-
-          # Make our inputs available to the config (for importing modules)
           extraSpecialArgs = { inherit theme; };
         };
       };
@@ -103,7 +79,7 @@
       # Accessible via 'nix build'
       packages = eachDefaultSystemMap (system:
         # Propagate nixpkgs' packages, with our overlays applied
-        import nixpkgs { inherit system; overlays = attrValues overlays; }
+        import nixpkgs { inherit system; }
       );
 
       # Devshell for bootstrapping
