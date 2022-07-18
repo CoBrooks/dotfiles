6141047fd089894dca2eb03749f5a85f809e57ca
CoBrooks <cole.brooks@colebrooks.com>
2022-07-18 18:07:35 -0500

    Update neovim config

--- modules/home-manager/neovim/init.lua
+++ modules/home-manager/neovim/init.lua
@@ -1,13 +1,13 @@
-require "plugins"
+require('impatient')
 
 local cmd = vim.cmd
 local fn = vim.fn
 local g = vim.g
 local opt = vim.opt
 
-cmd[[filetype plugin on]]
-cmd[[set noshowmode]]
-cmd[[set termguicolors]]
+cmd "filetype plugin on"
+cmd "set noshowmode"
+cmd "set termguicolors"
 
 g.mapleader = ' '
 g.maplocalleader = ','
@@ -40,10 +40,143 @@ require('lualine').setup {
   },
   tabline = {
     lualine_a = {'buffers'},
-    lualine_b = {'branch'}, 
+    lualine_b = {},
     lualine_c = {},
     lualine_x = {},
     lualine_y = {},
-    lualine_z = {'tabs'},
+    lualine_z = {'branch'},
   },
+}
+
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+local luasnip = require('luasnip')
+
+opt.completeopt="menu,menuone,noselect"
+
+local cmp = require('cmp')
+
+local function border(hl_name)
+  return {
+    { "╭", hl_name },
+    { "─", hl_name },
+    { "╮", hl_name },
+    { "│", hl_name },
+    { "╯", hl_name },
+    { "─", hl_name },
+    { "╰", hl_name },
+    { "│", hl_name },
+  }
+end
+
+-- Disable Scrolling in Autocomplete Window --
+local cmp_window = require("cmp.utils.window")
+
+cmp_window.info_ = cmp_window.info
+cmp_window.info = function (self)
+  local info = self:info_()
+  info.scrollable = false
+  return info
+end
+
+-- Fix Completion Menu Background --
+cmd "hi CmpItemAbbr guibg=None"
+
+local lspkind = require('lspkind')
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  window = {
+    completion = {
+      border = border "CmpBorder",
+      winhighlight = "Normal:CmpPmenu,FloatBorder:CmpPmenu,CursorLine:PmenuSel,Search:None",
+    },
+    documentation = {
+      border = border "CmpDocBorder",
+    },
+  },
+  formatting = {
+    format = lspkind.cmp_format({
+      mode = "symbol_text",
+      menu = ({
+        buffer = "[Buffer]",
+        nvim_lsp = "[LSP]",
+        luasnip = "[LuaSnip]",
+        nvim_lua = "[Lua]",
+        latex_symbols = "[Latex]",
+      }),
+    }),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  }),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+    { name = "buffer" },
+  }),
+  cmp.setup.cmdline('/', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = "buffer" }
+    }),
+  }),
+  cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = "path" }
+    }, {
+      { name = "cmdline" }
+    }),
+  }),
+}
+
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local lspconfig = require('lspconfig')
+lspconfig["rust_analyzer"].setup {
+  capabilities = capabilities,
+  settings = {
+    ["rust-analyzer"] = { }
+  }
+};
+lspconfig["sumneko_lua"].setup {
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      runtime = {
+        version = "LuaJIT",
+        path = runtime_path,
+      },
+      diagnostics = {
+        globals = { 'vim' },
+      }
+    }
+  }
 }
--- modules/home-manager/neovim/neovim.nix
+++ modules/home-manager/neovim/neovim.nix
@@ -1,8 +1,6 @@
 { pkgs, config, theme, ... }: 
 let 
-  nvim-base16 = pkgs.vimUtils.buildVimPlugin {
-    name = "nvim-base16";
-    src = pkgs.fetchFromGitHub {
+  nvim-base16 = pkgs.vimUtils.buildVimPlugin { name = "nvim-base16"; src = pkgs.fetchFromGitHub {
       owner = "RRethy";
       repo = "nvim-base16";
       rev = "da2a27cbda9b086c201b36778e7cdfd00966627a";
@@ -10,8 +8,6 @@ let
     };
   };
 in {
-  home.packages = [ pkgs.rust-analyzer ];
-
   home.sessionVariables = {
     EDITOR = "nvim";
   };
@@ -20,21 +16,50 @@ in {
     enable = true;
     extraConfig = ''
       lua << EOF
-      ${builtins.readFile ./config/init.lua}
-      cmd[[colorscheme base16-${theme.name}]]
+      vim.cmd "colorscheme base16-${theme.name}"
+      ${builtins.readFile ./init.lua}
+      EOF
     '';
-    plugins = with pkgs.vimPlugins; [
-      nvim-base16
-      (nvim-treesitter.withPlugins (_: pkgs.tree-sitter.allGrammars))
-      vim-nix
-      lualine-nvim
-      nvim-cmp
-      indent-blankline-nvim
+    extraPackages = with pkgs; [
+      # LSPs
+      rust-analyzer 
+      sumneko-lua-language-server 
+      # Tree-sitter deps
+      tree-sitter nodejs
+      # Telescope deps
+      ripgrep fd
     ];
-  };
-
-  home.file.".config/nvim/plugins" = {
-    recursive = true;
-    source = ./config/plugins;
+    plugins = with pkgs.vimPlugins; [
+      # measure startup time
+      vim-startuptime
+      # speed up startup time
+      impatient-nvim
+      # color themes
+      nvim-base16
+      # syntax highlighting
+      (nvim-treesitter.withPlugins (_: pkgs.tree-sitter.allGrammars))
+      # nix syntax defs
+      vim-nix
+      # status and tab line
+      lualine-nvim
+      # indent line
+      indent-blankline-nvim
+      # LSP
+      nvim-lspconfig
+      # autocompletion
+      nvim-cmp
+      ## sources
+      cmp-nvim-lsp
+      cmp-buffer
+      cmp-path
+      cmp-cmdline
+      ## snippet engine
+      luasnip
+      cmp_luasnip
+      ## menu icons
+      lspkind-nvim
+      # File picker / search util
+      telescope-nvim
+    ];
   };
 }
